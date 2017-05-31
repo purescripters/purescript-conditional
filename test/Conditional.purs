cded0d34c4cdb27f9ff87fd2d7b7cf54082eaa55
Risto Stevcev <risto1@gmail.com>
2017-05-31 08:27:05 +0200

    Updated for 0.11

--- test/Conditional.purs
+++ test/Conditional.purs
@@ -1,13 +1,13 @@
 module Test.Conditional where
 
-import Prelude ((&&), (||), ($), (+), (*), Unit, bind)
+import Prelude ((&&), (||), ($), (+), (*), Unit, discard)
 import Conditional ((?))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff.Console (CONSOLE)
 import Test.Unit (describe, it)
 import Test.Unit.Main (runTest)
-import Test.Unit.Assert (shouldEqual) 
+import Test.Unit.Assert (shouldEqual)
 import Test.Unit.Console (TESTOUTPUT)
 
 
@@ -31,5 +31,5 @@ main :: forall e
 main = runTest do
   describe "ifelse" do
     it "should return the same value as the equivalent if else statement" $ do
-      x `shouldEqual` x' 
+      x `shouldEqual` x'
       y `shouldEqual` y'
